b8b34dd10d56d212b8efd4af38d2aa94b01783f5
Jeaner <jeanerufmg@gmail.com>
2023-01-27 16:48:46 -0300

    Week4

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
+//npm install @react-navigation/drawer
+//npx expo install react-native-gesture-handler react-native-reanimated
+
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import MenuItems from './components/MenuItems';
 import WelcomeScreen from './components/WelcomeScreen';
-import FeedbackForm from './components/FeedBackForm';
-import LoginScreen from './components/LoginScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <LoginScreen />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+          </Drawer.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
     </>
   );
 }
@@ -30,17 +40,3 @@ const styles = StyleSheet.create({
   footerContainer: { backgroundColor: '#333333' },
 });
 
-/*
-  return (
-    <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <MenuItems />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
-    </>
-  );
-
-*/
